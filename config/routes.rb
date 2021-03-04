a4f3a7879c68458378ce661608214af7c5649d0a
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-04 03:18:58 -0700

    Remove friend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   
@@ -7,4 +8,10 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
 
   get 'search_stock', to: 'stocks#search'
+
+  get 'my_friends', to: 'users#my_friends'
+
+  get 'search_friend', to: 'users#search'
+
+  resources :friendships, only: [:create, :destroy]
 end
