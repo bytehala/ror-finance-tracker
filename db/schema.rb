b1f1418bf196b14c1a26467a54686c9304a10d43
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-04 01:41:19 -0700

    Add stocks table view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_072657) do
+ActiveRecord::Schema.define(version: 2021_03_04_082703) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_03_04_072657) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_072657) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
