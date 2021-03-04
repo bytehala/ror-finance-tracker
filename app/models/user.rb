2b34c6f165c38c6dcabdfca6201632d2f8c98528
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-03-04 02:28:07 -0700

    How to make link_to into a block

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,9 @@ class User < ApplicationRecord
     return false unless stock
     stocks.where(id: stock.id).exists?
   end
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Nameless"
+  end
 end
